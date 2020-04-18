402cdd27d7e08249631fd6498f33c2add4a5b6dc
James Reed <jamesreed@fb.com>
2020-04-17 19:53:28 -0700

    [WIP][quantization] Use torchbind for Linear PackedParams
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/34140
    
    Differential Revision: [D20229168](https://our.internmc.facebook.com/intern/diff/D20229168/)
    ghstack-source-id: 102426056

---
+++ aten/src/ATen/native/quantized/cpu/packed_params.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ATen/core/ivalue.h>
+
+struct LinearPackedParamsBase : public torch::jit::CustomClassHolder {
+  virtual at::Tensor apply(
+      at::Tensor input,
+      double output_scale,
+      int64_t output_zero_point) = 0;
+  virtual at::Tensor apply_relu(
+      at::Tensor input,
+      double output_scale,
+      int64_t output_zero_point) = 0;
+
+  virtual at::Tensor apply_dynamic(at::Tensor input) = 0;
+  virtual at::Tensor apply_dynamic_relu(at::Tensor input) = 0;
+
+  virtual std::tuple<at::Tensor, c10::optional<at::Tensor>> unpack() = 0;
+
+  virtual c10::optional<at::Tensor> bias() = 0;
+};
